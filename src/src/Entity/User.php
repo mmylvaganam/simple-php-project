468c664b02f8af15ce9ad8839b15684e185a5aee
michael.mylvaganam <michael.mylvaganam@objective.com>
2024-12-05 15:25:29 +1300

    updates

---
+++ src/src/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(type: Types::GUID)]
+    private ?string $RecordUUID = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getRecordUUID(): ?string
+    {
+        return $this->RecordUUID;
+    }
+
+    public function setRecordUUID(string $RecordUUID): static
+    {
+        $this->RecordUUID = $RecordUUID;
+
+        return $this;
+    }
+}
